f5c67e70e5453bf8e93440f4360d5ba6e86e3f3b
juhi1010 <rajanijuhi02@gmail.com>
2025-04-13 01:54:46 +0530

    login register

---
+++ src/main/java/com/juhi/spe_major/SpeMajorApplication.java
@@ -0,0 +1,13 @@
+package com.juhi.spe_major;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeMajorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeMajorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juhi/spe_major/SpeMajorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhi.spe_major;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeMajorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
